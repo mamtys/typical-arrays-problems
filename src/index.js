d629affe6bae3a6ffffa57a103399eb8b66f8e37
mamtys <mamtysman@gmail.com>
2020-03-02 13:26:53 +0300

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
+const _isValid = array => {
+  if (!array || !array.length) {
+    return false;
+  }
+
+  return true;
+}
+
 exports.min = function min (array) {
-  return 0;
+  if (!_isValid(array)) {
+    return 0;
+  }
+
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!_isValid(array)) {
+    return 0;
+  }
+
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!_isValid(array)) {
+    return 0;
+  }
+
+  const arraySum = array.reduce((sum, cur) => sum + cur, 0);
+
+  return arraySum/array.length;
 }
+
+
+
